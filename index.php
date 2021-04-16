01cd08b32aa161f2a93ef28c7b8ca575289da6e8
Felipe Reyes <felipedejesus.reyespacheco@gmail.com>
2021-04-15 22:27:40 -0500

    Se ha agrega el javascript, alert

--- index.php
+++ index.php
@@ -9,12 +9,15 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>Prueba PHP</title>
         <link rel="stylesheet" href="css/estilos.css">
+        <script src="js/app.js">
+        
+        </script>
     </head>
     
         <h1>Título</h1>
         <?php
         echo "Hola mundo!";
         ?>
-       
+        <input type="button" value="Picale aquí" onclick="saludo()">
     
 </html>
--- js/app.js
+++ js/app.js
@@ -1,22 +1,3 @@
-console.log('Hola mundo');/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-let miCanvas = document.querySelector('#pizarra');
-let lineas = [];
-let correccionX = 0;
-let correccionY = 0;
-let pintarLinea = false;
-// Marca el nuevo punto
-let nuevaPosicionX = 0;
-let nuevaPosicionY = 0;
-
-let posicion = miCanvas.getBoundingClientRect()
-correccionX = posicion.x;
-correccionY = posicion.y;
-
-miCanvas.width = 500;
-miCanvas.height = 500;
+function saludo(){
+    alert("Hola a todos!");
+}
